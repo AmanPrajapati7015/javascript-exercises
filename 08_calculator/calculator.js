e9f0f59cc546c4bd1b3a988d02fb5630adbb9019
Aman <aman.prajapati7022@gmail.com>
2023-06-27 22:10:39 +0530

    08 Calculator exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (+a)+(+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b ) {
+  return (a)-(b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((p, c)=>p+c, 0);
 };
 
 const multiply = function() {
-
+  nos = Array.from(arguments);
+  return nos.reduce((p, c)=>p*c, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return (a)**(b);
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let fact = 1;
+  for (let i = 1; i<= n;i++){
+    fact *=i 
+  }
+  return fact;
 	
 };
 
