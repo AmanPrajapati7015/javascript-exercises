f84bca1c881f614bc84e7e0437ceb67feb312ab2
Aman <aman.prajapati7022@gmail.com>
2023-06-27 22:48:37 +0530

    10 fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    let arr = [0, 1]
+    if (n < 0) return "OOPS";
+    
+    for(let i = 1; i<n; i++){
+        arr.push(arr[i]+arr[i-1]);
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
