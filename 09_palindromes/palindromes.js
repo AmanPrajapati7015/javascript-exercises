2d519f01b7fe18594edb8bde3b6fdf4fc9c8b157
Aman <aman.prajapati7022@gmail.com>
2023-06-27 22:37:11 +0530

    09 Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let toCheck = input.toLowerCase();
+    toCheck = toCheck.replaceAll("!", '');
+    toCheck = toCheck.replaceAll(".", '');
+    toCheck = toCheck.replaceAll(",", '');
+    toCheck = toCheck.replaceAll(' ','');
+
+
+    let len = toCheck.length;
+    let till = Math.floor(len/2);
+    let isPalindrome = true;
+    for (let i = 0; i<till;i++){
+        if (toCheck[i] != toCheck[len-1-i]){
+            isPalindrome = false;
+            break
+        }
+    }
+    return isPalindrome;
 
 };
 
